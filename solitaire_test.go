a60b49500fef368df97d115b82518edfb28024f3
Isabelle Miller <izzy@stardust.fm>
2014-03-21 11:36:11 -0400

    Fleshing out the stream manipulation step

--- solitaire_test.go
+++ solitaire_test.go
@@ -2,7 +2,16 @@ package solitaire
 
 import (
 	"testing"
+	"fmt"
 )
+
+func createCardList() []uint8 {
+	var d []uint8
+	for i:= uint8(0); i < 54; i++ {
+		d = append(d, i)
+	}
+	return d
+}
 
 func TestNewDeck(t *testing.T) {
 	d := NewDeck("")
@@ -15,16 +24,24 @@ func TestNewDeck(t *testing.T) {
 	}
 }
 
+func TestCountedCut(t *testing.T) {
+	var d Deck
+	d.cards = createCardList()
+	d = countedCut(d, 'A')
+	fmt.Println(d)
+	fmt.Println(countedCut(d, 'Z'))
+}
+
 func TestEncrypt(t *testing.T) {
-	pt := "hello, world"
-	var deck Deck
-	var ct string
-	ct, err := Encrypt(pt, deck)
-	if err != nil {
-		t.Error("Unexpected error: ", err)
-	} else if ct != "hello, world" {
-		t.Error("Got ", ct)
-	}
+//	pt := "hello, world"
+//	var deck Deck
+//	var ct string
+//	ct, err := Encrypt(pt, deck)
+//	if err != nil {
+//		t.Error("Unexpected error: ", err)
+//	} else if ct != "hello, world" {
+//		t.Error("Got ", ct)
+//	}
 }
 
 func TestDecrypt(t *testing.T) {
